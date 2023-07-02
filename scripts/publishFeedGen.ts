55c0df3db5b7aa452a5da129b3e9dc089ae8c7ef
ratgrimes <114757935+ratgrimes@users.noreply.github.com>
2023-07-02 12:56:12 -0400

    Update publishFeedGen.ts

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -8,24 +8,24 @@ const run = async () => {
 
   // YOUR bluesky handle
   // Ex: user.bsky.social
-  const handle = ''
+  const handle = 'ratgrimes.bsky.social'
 
   // YOUR bluesky password, or preferably an App Password (found in your client settings)
   // Ex: abcd-1234-efgh-5678
-  const password = ''
+  const password = '4hb2-a4hb-mck2-a6kx'
 
   // A short name for the record that will show in urls
   // Lowercase with no spaces.
   // Ex: whats-hot
-  const recordName = ''
+  const recordName = 'one-piece'
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = ''
+  const displayName = 'One Piece'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
-  const description = ''
+  const description = 'One Piece fanart, discussions, memes, etc'
 
   // (Optional) The path to an image to be used as your feed's avatar
   // Ex: ~/path/to/avatar.jpeg
